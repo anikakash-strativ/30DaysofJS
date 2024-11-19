86467bf81a47d2ed81adacbace90bfd5321c3524
anikakash01 <anik.dash@strativ.se>
2024-11-19 18:05:49 +0600

    Day-1 reviewed & Day-2 started

---
+++ Day-1/variables.js
@@ -0,0 +1,28 @@
+let stringVariable = "this is string"
+console.log(stringVariable)
+console.log('Type of the variable is : ', typeof stringVariable)
+console.log('\n');
+
+let check = true;
+console.log(check)
+console.log('Type of the variable is : ', typeof check)
+console.log('\n');
+
+let undiVal ;
+console.log(undiVal);
+console.log('Type of the variable is : ', typeof undiVal)
+console.log('\n');
+
+let nulVal = null;
+console.log(null);
+console.log('Type of the variable is : ', typeof nulVal)
+console.log('\n');
+
+
+const obj = {
+ 
+}
+
+console.log(typeof obj)
+
+console.log(Object.keys(obj).length)
