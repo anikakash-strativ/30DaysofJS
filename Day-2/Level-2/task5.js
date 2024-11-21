5a2f9411566368999f8a521f53bf2c0bc247583f
anikakash01 <anik.dash@strativ.se>
2024-11-21 13:19:29 +0600

    Day2 - Level2 - Task5

---
+++ Day-2/Level-2/task5.js
@@ -0,0 +1,8 @@
+let word1 = "python";
+let word2 = "jargon";
+
+let isFoundInPython = word1.includes("on");
+let isFoundInJargon = word2.includes("on");
+
+console.log(`'on' in python: ${isFoundInPython}`);
+console.log(`'on' in jargon: ${isFoundInJargon}`);
