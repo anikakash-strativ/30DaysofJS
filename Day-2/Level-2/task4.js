b64cbbceb439172f507cb59cec414f36fce80549
anikakash01 <anik.dash@strativ.se>
2024-11-21 13:14:42 +0600

    Day2 - Level2 - Task4

---
+++ Day-2/Level-2/task4.js
@@ -0,0 +1,11 @@
+let gForce = '9.8'
+
+if(gForce === 10){
+    console.log("same")
+}else {
+    console.log("not same");
+}
+
+let gForces = Math.round(parseFloat(gForce));
+
+console.log(gForces);
