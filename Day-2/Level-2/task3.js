4c63c7bc8840dadadc203ec33732725cb8771aec
anikakash01 <anik.dash@strativ.se>
2024-11-21 13:08:01 +0600

    Day2 - Level2 - Task3

---
+++ Day-2/Level-2/task3.js
@@ -0,0 +1,15 @@
+let str = '10';
+let num = 10;
+
+if(str === num){
+    console.log("Equal")
+}else{
+    console.log("These are not same!")
+}
+
+let numInt = parseInt(str);
+
+if(str == num){
+    console.log("Now they are Equal")
+}
+
