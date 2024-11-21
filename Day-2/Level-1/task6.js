b8dad8746d898d45d973cd76e2bfe4892be588f8
anikakash01 <anik.dash@strativ.se>
2024-11-21 09:17:24 +0600

    Day-2 - Level1 - Task6

---
+++ Day-2/Level-1/task6.js
@@ -0,0 +1,9 @@
+let challenge = "30 Days of JavaScript";
+
+
+// sub str take starting index and the size or sub string
+console.log(challenge.substr(0,2));
+
+// sub string take 2 index. starting and ending (end idx is excluded)
+
+console.log(challenge.substring(0,3))
