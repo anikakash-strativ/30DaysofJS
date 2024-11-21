e6bc290a3d2979c91df90808b1a66b5cc545cdb8
anikakash01 <anik.dash@strativ.se>
2024-11-21 14:12:27 +0600

    Day2 - Level2 - Task9

--- Day-2/Level-2/task9.js
+++ Day-2/Level-2/task9.js
@@ -3,4 +3,7 @@ let mn = 0, mx = 255;
 let num = Math.round(Math.random()*(mx-mn)+mn);
 console.log(num)
 
-// if we change the round to floor then it will generate less then max range. 
+// Can be done both way
+
+num = Math.round(Math.random()*mx);
+console.log(num)
