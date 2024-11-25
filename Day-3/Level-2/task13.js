c83cce2992f763985835d07f3f6a07d67b7e27a7
anikakash01 <anik.dash@strativ.se>
2024-11-25 11:52:26 +0600

    feat(date time) : human readable date time formating

--- Day-3/Level-2/task13.js
+++ Day-3/Level-2/task13.js
@@ -11,3 +11,24 @@ if(age >= 18){
     const yearToWait = 18-age;
     console.log(`you are ${age} years old. and you have to wait ${yearToWait}.`)
 }
+
+
+// Human readable date time:
+
+const now = new Date();
+
+// get all individual date and time.
+
+const year = now.getFullYear();
+const month = String(now.getMonth()+1).padStart(2, '0');
+const date = String(now.getDate()).padStart(2,'0');
+const hours = String(now.getHours()).padStart(2,'0');
+const minutes = String(now.getMinutes()).padStart(2,'0');
+
+const format1 = `${year}-${month}-${date} ${hours}:${minutes}`
+const format2 = `${date}-${month}-${year} ${hours}:${minutes}`
+const format3 = `${date}/${month}/${year} ${hours}:${minutes}`
+
+console.log(format1)
+console.log(format2)
+console.log(format3)
