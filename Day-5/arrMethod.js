b3fae52e74b39ddcd4aeae961a2bc95ef535918b
anikakash01 <anik.dash@strativ.se>
2024-11-25 15:48:44 +0600

    feat (array method) : concatenating & index find & join

--- Day-5/arrMethod.js
+++ Day-5/arrMethod.js
@@ -10,10 +10,51 @@ console.log(eight0values) // [0, 0, 0, 0, 0, 0, 0, 0]
 
 // Concatenating : 
 
-const firstList = [1, 2, 3];
+const firstList = [6, 2, 3];
 
 const secondList = [4, 5, 6];
 
 const thirdList = firstList.concat(secondList);
 
 console.log(thirdList);
+
+// size : 
+console.log(thirdList.length);
+
+// indexOf
+
+console.log(thirdList.indexOf(6));
+
+const numbers = [1, 2, 3, 4, 5, 3, 1, 2]
+
+console.log(`lastIndexOf : ${numbers.lastIndexOf(2)}`) // 7
+
+
+const webTechs = [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'React',
+    'Redux',
+    'Node',
+    'MongoDB'
+  ] // List of web technologies
+  
+  console.log(webTechs.includes('Node'))  // true
+  console.log(webTechs.includes('C'))     // false
+
+console.log(numbers.toString()) // 1,2,3,4,5
+console.log(numbers)
+
+console.log(" array joints: --------")
+
+console.log(numbers.join()) // 1,2,3,4,5
+
+const names = ['Asabeneh', 'Mathias', 'Elias', 'Brook']
+
+console.log(names.join()) // Asabeneh,Mathias,Elias,Brook
+console.log(names.join('')) //AsabenehMathiasEliasBrook
+console.log(names.join(' ')) //Asabeneh Mathias Elias Brook
+console.log(names.join(', ')) //Asabeneh, Mathias, Elias, Brook
+console.log(names.join(' ,')) 
+console.log(names.join(' # ')) //Asabeneh # Mathias # Elias # Brook
