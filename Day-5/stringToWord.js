575d44fcafec9dc90d909b4da1a5f93e395051e1
anikakash01 <anik.dash@strativ.se>
2024-11-25 16:31:34 +0600

    feat (array) : array add from front & back, edit and remove

--- Day-5/stringToWord.js
+++ Day-5/stringToWord.js
@@ -2,3 +2,36 @@ let text = 'I love teaching and empowering people. I teach HTML, CSS, JS, React,
 const words = text.replace(/[^a-zA-Z0-9\s]/g,'').split(' ');
 console.log(words)
 console.log(words.length)
+
+
+console.log("\n ----------- \n");
+
+const shoppingCart = ['Milk', 'Coffee', 'Tea', 'Honey'];
+
+// add meat
+if(!shoppingCart.includes['Meat']){
+    shoppingCart.unshift("Meat");
+}
+
+// add sugar from end;
+
+if(!shoppingCart.includes['Sugar']){
+    shoppingCart.push("Sugar");
+}
+
+const isAllergicToHoney = true;
+
+if(isAllergicToHoney){
+    const honeyIndex = shoppingCart.indexOf("Honey");
+
+    if(honeyIndex !== -1){
+        shoppingCart.splice(honeyIndex, 1);
+    } 
+}
+
+const teaIndex = shoppingCart.indexOf('Tea');
+if(teaIndex !== -1){
+    shoppingCart[teaIndex] = 'Green Tea';
+}
+
+console.log(shoppingCart)
