facd9784762091a99e435c8f54c36630eb702f51
anikakash01 <anik.dash@strativ.se>
2024-11-25 15:59:22 +0600

    feat (array) : includes any keyword check

--- Day-5/taskL1.js
+++ Day-5/taskL1.js
@@ -3,3 +3,13 @@ let itCompanies = ["Facebook", "Google", "Microsoft", "Apple", "IBM", "Oracle", 
 itCompanies.forEach(company => {
     console.log(company);
 })
+
+console.log("\n")
+
+let searchCompany = "Google";
+
+if(itCompanies.includes(searchCompany)){
+    console.log(`${searchCompany} is found.`);
+}else{
+    console.log(`${searchCompany} is not found.`);
+}
