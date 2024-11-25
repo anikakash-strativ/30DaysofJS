4c1a4afe566fb88a4d01899bb6c82397c28b6052
anikakash01 <anik.dash@strativ.se>
2024-11-25 12:42:21 +0600

    feat (array method) : array create and split.

---
+++ Day-5/array.js
@@ -0,0 +1,15 @@
+let js = 'JavaScript'
+const charsInJavaScript = js.split('');
+
+console.log(charsInJavaScript);
+
+let companiesString = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon'
+const companies = companiesString.split(',')
+
+console.log(companies);
+
+
+let text = "I love teaching and empowering people. I teach HTML, CSS, JS, React, Python.";
+
+const words = text.split(' ');
+console.log(words);
