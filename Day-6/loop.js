4cfaf3d438c875b897d22b1d1646ff1f81be87e2
anikakash01 <anik.dash@strativ.se>
2024-11-26 11:29:08 +0600

    feat (loop) : Basic loop for, while, do while

---
+++ Day-6/loop.js
@@ -0,0 +1,19 @@
+console.log("For loop ---------- \n")
+for(let i=0; i<=10; i++){
+    console.log(i);
+}
+
+
+console.log("While loop ---------- \n")
+let i=0;
+while(i<=10){
+    console.log(i);
+    i++;
+}
+
+console.log("Do loop ---------- \n")
+i=0;
+do{
+    console.log(i);
+    i++;
+}while(i<=10);
