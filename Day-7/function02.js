573d6e7dc3276551b209c62fc0ec8fa0618eb220
anikakash01 <anik.dash@strativ.se>
2024-11-27 15:23:28 +0600

    feat(function) : Quadratic function

---
+++ Day-7/function02.js
@@ -0,0 +1,26 @@
+// 2
+
+function solveQuadratic(a = 1, b = 0, c = 0) {
+    const res = b * b - 4 * a * c;
+  
+    if (res > 0) {
+      const root1 = (-b + Math.sqrt(res)) / (2 * a);
+      const root2 = (-b - Math.sqrt(res)) / (2 * a);
+      return { root1, root2 }; 
+    } else if (res === 0) {
+      const root = -b / (2 * a);
+      return { root };
+    } else {
+      return {}; 
+    }
+  }
+  
+  console.log(solveQuadratic())           
+  console.log(solveQuadratic(1, 4, 4))      
+  console.log(solveQuadratic(1, -1, -2)) 
+  console.log(solveQuadratic(1, 7, 12))     
+  console.log(solveQuadratic(1, 0, -4))     
+  console.log(solveQuadratic(1, -1, 0))     
+  
+
+  
