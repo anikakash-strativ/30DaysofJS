ecfa5b34345ff1a703326b4cc532e38828043636
anikakash01 <anik.dash@strativ.se>
2024-12-03 16:13:49 +0600

    feat(level2/destructuring_and_spreading): level 2 done

--- Day-11/lelve1.js
+++ Day-11/lelve1.js
@@ -13,7 +13,9 @@ const rectangle = {
 const { width, height, area, perimeter } = rectangle;
 console.log(width, height, area, perimeter)
 
-// 3
+// Level 2:
+
+
 const users = [
     {
       name:'Brook',
@@ -57,11 +59,16 @@ const users = [
       skills:['HTM', 'CSS', 'JS'],
       age:20
     }
-    ]
+    ];
+
+
+    // Task 1: 
 
     users.forEach(({name, scores, skills, age})=>{
         console.log(`Keys: ${name}, ${scores}, ${skills}, ${age}`);
     })
+
+    // Task 2:
 
     let lessSkilled = users.filter(({skills})=>{
         return skills.length <2;
@@ -69,3 +76,77 @@ const users = [
 
     console.log(`Less then 2 skills :`);
     console.log(lessSkilled)
+
+// Level 3:
+
+const countries = [
+    {
+      name: "Finland",
+      capital: "Helsinki",
+      population: 5530719,
+      languages: ["Finnish", "Swedish"]
+    },
+    {
+      name: "Estonia",
+      capital: "Tallinn",
+      population: 1328976,
+      languages: ["Estonian"]
+    },
+    {
+      name: "Sweden",
+      capital: "Stockholm",
+      population: 10353442,
+      languages: ["Swedish"]
+    }
+  ];
+
+  console.log("----------\n")
+  // Task 1:
+  countries.forEach(({name, capital, population, languages})=>{
+    console.log(`Country: ${name}`);
+    console.log(`Capital: ${capital}`);
+    console.log(`Population: ${population}`);
+    console.log(`Languages: ${languages.join(", ")}`);
+    console.log("-----------------------");
+  })
+
+  // Task 2:
+const student = ['David', ['HTM', 'CSS', 'JS', 'React'], [98, 85, 90, 95]];
+const [name, skills, [, , jsScore, reactScore]] = student;
+
+// console.log(name);   
+// console.log(skills);      
+// console.log(jsScore);     
+// console.log(reactScore);  
+
+// Task 3:
+
+const students = [
+    ['David', ['HTM', 'CSS', 'JS', 'React'], [98, 85, 90, 95]],
+    ['John', ['HTM', 'CSS', 'JS', 'React'], [85, 80, 85, 80]]
+  ]
+
+// function convertArrayToObject (st){
+//     let arr = [];
+//     st.forEach(([name, skills, scores])=>{
+//         arr.push({
+//             name,
+//             skills, 
+//             scores,
+//        });
+//     }) 
+//     return arr;
+// }
+// console.log(convertArrayToObject(students));
+
+// 2nd way:
+function convertArrayToObject (st){
+    return st.map(([name, skills, scores])=>
+        ({
+            name,
+            skills, 
+            scores,
+       })
+    ) 
+}
+console.log(convertArrayToObject(students));
