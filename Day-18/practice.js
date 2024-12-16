4661095b22d74ed98dc1929f9e3b052749ad4b71
anikakash01 <anik.dash@strativ.se>
2024-12-16 20:28:51 +0600

    feat(add) : featch api

--- Day-18/practice.js
+++ Day-18/practice.js
@@ -2,6 +2,7 @@
 
 const countriesAPI = 'https://restcountries.com/v2/all'
 
+
 fetch(countriesAPI)
     .then(response => response.json())
     .then(data => {
@@ -27,14 +28,57 @@ fetch(countriesAPI)
 
 const catsAPI = 'https://api.thecatapi.com/v1/breeds'
 
+// fetch(catsAPI)
+//     .then(response => response.json())
+//     .then(data => {
+//         data.forEach(cat => {
+//             const name = cat.name;
+
+//             console.log(`Name of a Cat : ${name}`)
+//             console.log('-------------------');
+//         })
+//     })
+//     .catch(error => console.error('Error fetching data:', error))
+
+
+// Task 3:
+
+
+// SUB Task 1:
+
 fetch(catsAPI)
     .then(response => response.json())
     .then(data => {
-        data.forEach(cat => {
-            const name = cat.name;
 
-            console.log(`Name of a Cat : ${name}`)
+        data.forEach(cat => {
+            const weightMetric = cat.weight.metric;
+            const [min, mx] = weightMetric.split(' - ').map(Number);
+            const averageWeight = (min+mx)/2;
+
+            console.log(`Name: ${cat.name}`);
+            console.log(`Average Weight: ${averageWeight} kg`);
             console.log('-------------------');
         })
     })
     .catch()
+
+
+// SUB Task 2:
+fetch(countriesAPI)
+    .then(response => response.json())
+    .then(countryData =>{
+        const sortedCountries = countryData
+              .filter(country => country.area)
+              .sort((a,b)=> b.area - a.area);
+        const to10LagestCountry = sortedCountries.slice(0,10);
+
+        console.log("To 10 Lagest Countries by area: ");
+        to10LagestCountry.forEach((country, index)=>{
+            console.log(`${index+1}. ${country.name} - ${country.area} km²`)
+        })
+    })
+    .catch(error => console.error('Error:', error))
+
+// Sub Task 3:
+
+// Just store the language in a set. 
